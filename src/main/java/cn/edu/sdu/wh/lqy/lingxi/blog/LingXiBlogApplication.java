f83b9f8300b162f52eb7b48384639481c48b66ca
liqingyong <liqingyong02@meituan.com>
2018-04-19 23:49:40 +0800

    refactors

--- src/main/java/cn/edu/sdu/wh/lqy/lingxi/blog/LingXiBlogApplication.java
+++ src/main/java/cn/edu/sdu/wh/lqy/lingxi/blog/LingXiBlogApplication.java
@@ -1,10 +1,12 @@
 package cn.edu.sdu.wh.lqy.lingxi.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableTransactionManagement
 public class LingXiBlogApplication {
 
