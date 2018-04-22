96c3926b0d46c78a31ac3f846a21a99129145fe4
liqingyong <liqingyong02@meituan.com>
2018-04-22 13:02:05 +0800

    refactor

---
+++ src/main/java/cn/edu/sdu/wh/lqy/lingxi/blog/model/dto/ErrorCode.java
@@ -0,0 +1,13 @@
+package cn.edu.sdu.wh.lqy.lingxi.blog.model.dto;
+
+/**
+ * 错误提示
+ */
+public interface ErrorCode {
+
+    /**
+     * 非法请求
+     */
+    String BAD_REQUEST = "BAD REQUEST";
+
+}
---
+++ src/main/java/cn/edu/sdu/wh/lqy/lingxi/blog/model/dto/MetaDto.java
@@ -0,0 +1,16 @@
+package cn.edu.sdu.wh.lqy.lingxi.blog.model.dto;
+
+import cn.edu.sdu.wh.lqy.lingxi.blog.model.Vo.Meta;
+
+public class MetaDto extends Meta {
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
